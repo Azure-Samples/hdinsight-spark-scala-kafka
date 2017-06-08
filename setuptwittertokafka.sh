a93c40cd2a2bff6068c5183b9d5e94023e23ccca
Larry Franks <larry.franks@microsoft.com>
2017-06-08 10:25:33 -0400

    adding progress bar

--- setuptwittertokafka.sh
+++ setuptwittertokafka.sh
@@ -2,3 +2,4 @@
 
 /usr/bin/anaconda/bin/conda install -y -c conda-forge tweepy
 /usr/bin/anaconda/bin/conda install -y -c conda-forge kafka-python
+/usr/bin/anaconda/bin/conda install -y -c conda-forge progressbar2
